2fb0aaf4501a467329140a58d3a0fcbb1e670c63
maxime-code <maxime.roquelle@isen-ouest.yncrea.fr>
2023-06-19 16:01:43 +0200

    kmeans ez

--- maxime.py
+++ maxime.py
@@ -3,8 +3,20 @@ import random
 import math
 from statistics import mean 
 import json
+import plotly.express as px
+import matplotlib.pyplot as plt
+from numpy import sin, cos, arccos, pi, round
+from sklearn.cluster import KMeans
+import time
 
-df = pd.read_csv('data/stat_acc_V3.csv', sep =";")
+
+def rad2deg(radians):
+    degrees = radians * 180 / pi
+    return degrees
+
+def deg2rad(degrees):
+    radians = degrees * pi / 180
+    return radians
 
 # script sur l'apprentissage non supervisé
 def appartient_cluster(lat,lon, clusters):
@@ -17,15 +29,17 @@ def appartient_cluster(lat,lon, clusters):
 
     min_dist = min(dist_table)
     index = min_dist[1]
+    return index
+
     # créer un fichier json qui contient les coordonnées du cluster auquel appartient le point
-    aDict = {"lat_centroides":clusters[index][0], "lon_centroides":clusters[index][1]}
-    jsonString = json.dumps(aDict)
-    jsonFile = open("cluster.json", "w")
-    jsonFile.write(jsonString)
-    jsonFile.close()
+    # aDict = {"lat_centroides":clusters[index][0], "lon_centroides":clusters[index][1]}
+    # jsonString = json.dumps(aDict)
+    # jsonFile = open("cluster.json", "w")
+    # jsonFile.write(jsonString)
+    # jsonFile.close()
 
 
-def KMEANS(k,df, nb_iteration = 10):
+def KMEANS(k,df, nb_iteration = 10,methode=3,mink=2):
 
     # get the first centroids coordinates
     clusters = [] # contient les coordonnées des clusters
@@ -44,7 +58,34 @@ def KMEANS(k,df, nb_iteration = 10):
             dist_table = []
             cluster_index = 0
             for j in range(k):
-                dist = [math.sqrt((clusters[j][0]-latitude[i])**2+(clusters[j][1]-longitude[i])**2),cluster_index]
+                match methode:
+                    case 1:
+                        dist = [abs((clusters[j][0]-latitude[i])+(clusters[j][1]-longitude[i]))**(1/mink),cluster_index]
+                    case 2:
+                        dist = [abs((clusters[j][0]-latitude[i])+(clusters[j][1]-longitude[i])),cluster_index]
+                    case 3:
+                        dist = [math.sqrt((clusters[j][0]-latitude[i])**2+(clusters[j][1]-longitude[i])**2),cluster_index]
+                    case 4: # distance de haversine
+                        theta = clusters[j][1] - longitude[i]
+    
+                        distance = 60 * 1.1515 * rad2deg(
+                            arccos(
+                                (sin(deg2rad(latitude[i])) * sin(deg2rad(clusters[j][0]))) + 
+                                (cos(deg2rad(latitude[i])) * cos(deg2rad(clusters[j][0])) * cos(deg2rad(theta)))
+                            )
+                        )
+                        # if unit == 'miles':
+                        dist = [round(distance, 2),cluster_index]
+                        
+                        # if unit == 'kilometers':
+                            # return round(distance * 1.609344, 2)
+                # ecrit la distance de manahttan entre le point et le cluster j dans la table dist_table
+                # dist = [abs((clusters[j][0]-latitude[i])+(clusters[j][1]-longitude[i]))**(1/mink),cluster_index]
+                # manatthan
+                # dist = [abs((clusters[j][0]-latitude[i])+(clusters[j][1]-longitude[i])),cluster_index]
+                # euclidienne
+                # dist = [math.sqrt((clusters[j][0]-latitude[i])**2+(clusters[j][1]-longitude[i])**2),cluster_index]
+                # print(dist)
                 cluster_index += 1
                 dist_table.append(dist)
 
@@ -65,15 +106,76 @@ def KMEANS(k,df, nb_iteration = 10):
             clusters[m][1] = new_coord_lon
             table_lat = []
             table_lon = []
-        print("Coordonnées des clusters à l'iteration", p, " : " ,clusters)
+        # print("Coordonnées des clusters à l'iteration", p, " : " ,clusters)
     
     return clusters
-        
 
-        
-clusters = KMEANS(5,df, 2)
-print("final clusters : ", clusters)
 
-appartient_cluster(48.8566969,2.3514616,clusters)
+def fit_departement(df):
+    for i in range(len(df["latitude"])):
+        if(df["latitude"][i] < 40 ):
+            df.drop(i, inplace=True,axis=0)
+        else:
+            if(df["longitude"][i] < -10 or df["longitude"][i]> 20):
+                df.drop(i, inplace=True,axis=0)
 
+    df.reset_index(drop=True, inplace=True)
+    return df
+
+
+
+def plot_clusters(df,clusters,path):
+    palette = ['bo','ro','go','co','mo','yo','ko','wo']
+    for i in range(len(df["longitude"])):
+        index = appartient_cluster(df["latitude"][i],df["longitude"][i],clusters)
+        match index:
+            case 0:
+                plt.plot(df["longitude"][i], df["latitude"][i], palette[0],markersize=0.1)
+            case 1:
+                plt.plot(df["longitude"][i], df["latitude"][i], palette[1],markersize=0.1)
+            case 2:
+                plt.plot(df["longitude"][i], df["latitude"][i], palette[2],markersize=0.1)
+            case 3:
+                plt.plot(df["longitude"][i], df["latitude"][i], palette[3],markersize=0.1)
+            case 4:
+                plt.plot(df["longitude"][i], df["latitude"][i], palette[4],markersize=0.1)
+            case 5:
+                plt.plot(df["longitude"][i], df["latitude"][i], palette[5],markersize=0.1)
+            case 6:
+                plt.plot(df["longitude"][i], df["latitude"][i], palette[6],markersize=0.1)
+            case 7:
+                plt.plot(df["longitude"][i], df["latitude"][i], palette[7],markersize=0.1)
+            
+    for i in range(len(clusters)):
+        plt.plot(clusters[i][1], clusters[i][0], palette[i],markersize=10)
+
+    plt.ylabel('latitude et longitude')
+    plt.savefig(path)
+
+
+df = pd.read_csv('data/stat_acc_V3.csv', sep =";")  
+print("data loaded !")  
+df = fit_departement(df)
+print("data fitted !")
+st = time.time()
+clusters = KMEANS(5,df, 10,methode=1)
+et = time.time()
+elapsed_time = et - st
+print('Execution time manual:', elapsed_time, 'seconds')
+# print(clusters)
+# print("KMEANS ok !")
+# print("final clusters : ", clusters)
+print("plotting clusters ...")
+plot_clusters(df,clusters,'export/manuel.png')
+print("plotting done !")
+plt.close()
+st = time.time()
+kmeans = KMeans(n_clusters=5, random_state=0, n_init="auto").fit(df[["latitude","longitude"]])
+et = time.time()
+elapsed_time = et - st
+print('Execution time sklearn:', elapsed_time, 'seconds')
+
+# print(kmeans.cluster_centers_)
+plot_clusters(df,kmeans.cluster_centers_,'export/sklearn.png')
+plt.close()
 
