b316f0f71974510b2ab9825093f150190680f048
Binau02 <aubin.saunier@gmail.com>
2023-06-22 12:00:03 +0200

    knn.py

--- test.py
+++ test.py
@@ -1,12 +1,5 @@
-test = "[[1, 2], [3, 4], [5, 6]]"
+test = "srgdhfyjh"
 
-test = test.replace(" ", '')
-test = test.replace("[", '')
-test = test[:-2]
-test = test.split(sep = "],")
-for i in range(len(test)):
-  test[i] = test[i].split(sep = ",")
-  test[i][0] = float(test[i][0])
-  test[i][1] = float(test[i][1])
-
-print(test)
+print(type(test))
+if type(test) == type(""):
+    print("rlsgrsd")
